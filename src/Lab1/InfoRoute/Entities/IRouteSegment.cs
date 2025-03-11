deaa62bf028f304786b50397477ce3751f7bbd07
m1keee3 <skirdamisa@gmail.com>
2025-03-11 15:47:12 +0300

    remove: other labs

--- src/Lab1/InfoRoute/Entities/IRouteSegment.cs
+++
@@ -1,8 +0,0 @@
-﻿using Itmo.ObjectOrientedProgramming.Lab1.InfoTrain.Entities;
-
-namespace Itmo.ObjectOrientedProgramming.Lab1.InfoRoute.Entities;
-
-public interface IRouteSegment
-{
-    public RouteResult Passing(Train train);
-}
